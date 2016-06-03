c3e9db3516b8933ef51318f91405b2dadb2ad392
Philip Hayes <philiphayes@Philips-MacBook-Pro.local>
2016-06-02 22:48:41 -0400

    moved to fhc-ansible npm module

--- rhmap/ansible/library/fhc.js
+++ rhmap/ansible/library/fhc.js
@@ -1,28 +1,21 @@
 #!/usr/local/bin/node
 fs = require('fs');
-var target = require(process.env.FHMODULE_HOME+'/lib/target.js');
-var login = require(process.env.FHMODULE_HOME+'/lib/login.js');
-var mbaas = require(process.env.FHMODULE_HOME+'/lib/mbaas.js');
-var init = require(process.env.FHMODULE_HOME+'/lib/init.js');
-var environment = require(process.env.FHMODULE_HOME+'/lib/environment.js');
-var team = require(process.env.FHMODULE_HOME+'/lib/team.js');
-var project = require(process.env.FHMODULE_HOME+'/lib/project.js');
-var user = require(process.env.FHMODULE_HOME+'/lib/user.js');
+var ansiblefhc = require('ansible-fhc');
 
 // get arguments passed in by file
-init.getArgs(function(err, args){
+ansiblefhc.init.getArgs(function(err, args){
   var action;
   if (err){
     finish({err: err});
   } else {
     // load the fhc module.
-    init.fhLoad(function(err, success){
+    ansiblefhc.init.fhLoad(function(err, success){
       if (err ){
         finish({err: err});
       } else {
         action = args.action;
         // perform the action (first argument passed)
-        doAction(action, args);
+        ansiblefhc.process(action, args, finish);
       }
     });
   }
@@ -42,30 +35,6 @@ function finish(output, changed ){
   
 }
 
-function doAction(action, args){
-  //finish(args)
-  if (action == 'target'){
-    target.set(args['target'], finish);
-  }
-  else if (action == 'login'){
-    login.doLogin(args['username'], args['password'], finish);
-  }
-  else if (action == 'createMBaaSTarget'){
-    mbaas.create(args, finish);
-  }
-  else if (action == 'createEnvironment'){
-    environment.create(args, finish);
-  }
-  else if (action == 'createTeam'){
-    team.create(args, finish);
-  }
-  else if (action == 'createProject'){
-    project.create(args, finish);
-  }
-  else if (action == 'createUser'){
-    user.create(args, finish);
-  }
-}
 
 
 
